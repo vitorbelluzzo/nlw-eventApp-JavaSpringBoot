fe6361c643a0c2066dbed073f9c398e60dfa3d55
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2025-03-01 03:22:35 -0300

    add event controller and service methods for event management

--- src/main/java/br/com/ventidue/events/controller/EventController.java
+++ src/main/java/br/com/ventidue/events/controller/EventController.java
@@ -3,10 +3,7 @@ package br.com.ventidue.events.controller;
 import br.com.ventidue.events.model.Event;
 import br.com.ventidue.events.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,4 +22,9 @@ public class EventController {
     public List<Event> getAllEvents() {
         return  eventService.getAllEvents();
     }
+    @GetMapping("/events/{prettyName}")
+    public Event getEventByPrettyName(@PathVariable String prettyName) {
+        return  eventService.getByPrettyName(prettyName);
+    }
+
 }
