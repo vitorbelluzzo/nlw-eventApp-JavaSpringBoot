129668e647a788f57618178d24aa0a5c618faaab
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2025-04-01 13:35:02 -0300

    added user model

---
+++ src/main/java/br/com/ventidue/events/model/User.java
@@ -0,0 +1,40 @@
+package br.com.ventidue.events.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer id;
+    @Column(name = "user_name", length = 255, nullable = false)
+    private String name;
+
+    @Column(name = "user_email", length = 255, nullable = false, unique = true)
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
